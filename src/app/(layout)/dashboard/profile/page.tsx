be7c54ed419d0666ff323ac9f0ec422ceb184ba9
saiket-das <saiketdas02@gmail.com>
2024-12-05 18:17:06 +0800

    Create profile-form for profile page

--- src/app/(layout)/dashboard/profile/page.tsx
+++ src/app/(layout)/dashboard/profile/page.tsx
@@ -1,9 +1,13 @@
+import { ProfileForm } from "@/components/dashbaord/profile/profile-form";
 import React from "react";
 
 const Profile = () => {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <h1 className="text-lg font-semibold md:text-2xl">Profile</h1>
+      <div className="space-y-6 w-[600px]">
+        <ProfileForm />
+      </div>
     </main>
   );
 };
